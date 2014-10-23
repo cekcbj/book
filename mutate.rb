48e67dc40a38e3be60e5530ba18c8de71135836e
Bjones <cekcb@yahoo.com>
2014-10-23 08:44:50 -0500

    mutate

---
+++ mutate.rb
@@ -0,0 +1,28 @@
+def some_method( number)
+	number = 7
+end
+
+a=5
+some_method(a)
+puts a
+
+a=[1,2,3]
+
+def mutate (array)
+	array.pop
+end
+
+p "before mutate method: #{a}"
+mutate (a)
+p "After mutate method: #{a}"
+
+
+a=[1,2,3]
+
+def no_mutate (array)
+	array.last
+end
+
+p "before no_mutate method: #{a}"
+no_mutate (a)
+p "After no_mutate method: #{a}"
